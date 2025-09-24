ac62c64b3770e7659fbd2ec05f6248d6f124affa
Utu Hopiavuori <gitlab.com@mail.hopiavuori.net>
2025-09-24 18:31:14 +0300

    Arcs working

--- CV-SVG.py
+++ CV-SVG.py
@@ -5,7 +5,7 @@ import xml.dom.minidom
 import yaml
 max_x = 100
 max_y = 100
-circles = []
+elements = []
 output_file = './test.svg'
 
 def validate_date(iso_date="2000-01-01"):
@@ -16,18 +16,22 @@ def validate_date(iso_date="2000-01-01"):
         iso_date = validate_date(input())
     return date.fromisoformat(iso_date)
 
-def days_between(start_date = date.fromisoformat('2000-01-01'), 
-                 end_date = date(date.today().year,12,31)):
-    return (end_date - start_date).days
+# defaults
+start_date = validate_date("2000-01-01")
+end_date = date.today()
 
-def years_between(start_date = date.fromisoformat('2000-01-01'), 
-                 end_date = date(date.today().year,12,31)):
-    return math.ceil((end_date - start_date).days / 365.25)
+def days_between(start = start_date, 
+                 end = date(date.today().year,12,31)):
+    return (end - start).days
 
-def angle(start_date = date.fromisoformat('2000-01-01'), 
-                 end_date = date(date.today().year,12,31)):
+def years_between(start = start_date, 
+                 end = date(date.today().year,12,31)):
+    return math.ceil((end - start).days / 365.25)
+
+def angle(start = start_date, 
+                 end = date(date.today().year,12,31)):
     # print(days_between(start_date, end_date))
-    angle = 2 * math.pi * (days_between(start_date, end_date)) / days_between()
+    angle = 2 * math.pi * (days_between(start, end)) / days_between()
     # print(angle)
     if (angle == 0):
         return 360.0
@@ -38,7 +42,7 @@ def angle(start_date = date.fromisoformat('2000-01-01'),
 #       '/', days_between(), "d")
 #print (years_between(validate_date("2020-01-01"),validate_date("2025-09-24")), 
 #       '/', math.ceil(days_between() / 365.25) ,"a")
-print (angle(validate_date("2020-01-01"),date.today()), "degrees.")
+print (angle(validate_date("2020-01-01"), end_date), "degrees.")
 
 
 def circle(x, y, radius, width, color, fill, opacity=1.0):
@@ -48,21 +52,53 @@ def circle(x, y, radius, width, color, fill, opacity=1.0):
         opacity=opacity
     )
 
+def arc(radius, width, color, text_color, opacity=1, start=start_date, end=end_date, text=""):
+    x1 = radius
+    y1 = 0.0
+    x2 = math.cos(math.radians(angle(start, end))) * radius
+    y2 = math.sin(math.radians(angle(start,end))) * radius
+    rotation = angle(validate_date("2000-01-01"),start)
+    large_arc = bool(angle(start, end) > 180)
+    sweep = True
+
+    a = svg.Arc(rx=radius, ry=radius, angle=0, large_arc=large_arc,sweep=sweep, x=x2, y=y2)
+
+    a = f"M {x1:.1f} {y1:.1f} {a}"
+
+    yield svg.Path(d=a, stroke=color,stroke_width=width,fill='none', opacity=opacity, transform=f"rotate({rotation})")
 # Background
-circles.extend(circle(0,0,100,0,'none','#000000'))
+elements.extend(circle(0,0,100,1,'#000000','#000000'))
 
 
 # Arcs background
-circles.extend(circle(0,0,95,9,'#2C2C2C','none',0.7))
-circles.extend(circle(0,0,80,19,'#9C59D1','none'))
-circles.extend(circle(0,0,60,19,'#ffffff','none'))
-circles.extend(circle(0,0,40,19,'#FCF434','none'))
-circles.extend(circle(0,0,29.5,0,'none','#dddddd'))
+#elements.extend(circle(0,0,95,9,'#2C2C2C','none'))
+elements.extend(circle(0,0,80,19,'#9C59D1','none'))
+elements.extend(circle(0,0,60,19,'#ffffff','none'))
+elements.extend(circle(0,0,40,19,'#FCF434','none'))
+elements.extend(circle(0,0,29.5,0,'none','#dddddd'))
 
+# Arcs
+elements.extend(
+    arc(
+        95,9,"#2c2c2c","#cccccc",0.35,
+        validate_date("2000-01-01"), 
+        validate_date("2000-12-31"),
+        "2000"
+    )
+)
+
+elements.extend(
+    arc(
+        95,9,"#2c2c2c","#cccccc",0.75,
+        validate_date("2001-01-01"), 
+        validate_date("2001-12-31"),
+        "2001"
+    )
+)
 
 canvas = svg.SVG(
     viewBox=svg.ViewBoxSpec(-max_x, -max_y, 2 * max_x, 2 * max_y),
-    elements=circles,
+    elements=elements,
 )
 
 with open(output_file, "w") as f:

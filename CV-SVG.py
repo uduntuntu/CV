73b8fc8490d114d62f1bc53c83e62b6708317efc
Utu Hopiavuori <gitlab.com@mail.hopiavuori.net>
2025-09-25 01:28:26 +0300

    Example jobs and schools.

--- CV-SVG.py
+++ CV-SVG.py
@@ -52,7 +52,7 @@ def circle(x, y, radius, width, color, fill, opacity=1.0):
         opacity=opacity
     )
 
-def arc(radius, width, color, text_color, opacity=1, start=start_date, end=end_date, text=""):
+def arc(radius, width, color, text_color, opacity=1, start=start_date, end=end_date, text="",id="a"):
     x1 = radius
     y1 = 0.0
     x2 = math.cos(math.radians(angle(start, end))) * radius
@@ -70,13 +70,13 @@ def arc(radius, width, color, text_color, opacity=1, start=start_date, end=end_d
     a = f"M {x1:.1f} {y1:.1f} {a}"
 
     yield svg.Path(
-        d=a, id="a", stroke=color,stroke_width=width,fill='none', opacity=opacity, 
+        d=a, id=id, stroke=color,stroke_width=width,fill='none', opacity=opacity, 
         transform=f"rotate({rotation})"
     )
     
     yield svg.Text(
             text = svg.TextPath(
-                href=f"#a",
+                href=f"#{id}",
                 startOffset="50%",
                 text=f"{text}",
             ),
@@ -84,8 +84,7 @@ def arc(radius, width, color, text_color, opacity=1, start=start_date, end=end_d
             font_family="sans-serif",
             fill=text_color,
             text_anchor="middle",
-            dominant_baseline="middle",
-            transform=f"rotate({rotation})"
+            dominant_baseline="middle"
         )
     
 # Background
@@ -100,16 +99,99 @@ elements.extend(circle(0,0,40,19,'#FCF434','none'))
 elements.extend(circle(0,0,29.5,0,'none','#dddddd'))
 
 
-# Arcs
-elements.extend(
+# Year arcs
+for year in range(2000, date.today().year + 1):
+    if (year % 2 == 0): opacity = 0.35
+    else: opacity = 0.65
+
+    elements.extend(
+        arc(
+            95,9,"#2c2c2c","#cccccc",opacity,
+            validate_date(f"{year}-01-01"), 
+            validate_date(f"{year}-12-31"),
+            f"{year}",f"{year}"
+        )
+    )
+
+# Job arcs
+jobs = []
+jobs.extend(
     arc(
-        95,9,"#2c2c2c","#cccccc",0.35,
-        validate_date("2000-01-01"), 
-        validate_date("2000-12-31"),
-        "2000"
+        85.5,9,"#2c2c2c","#cccccc",0.35,
+        validate_date(f"2019-09-30"), 
+        validate_date(f"2024-03-31"),
+        "Suomen Rauhanliitto","job_1"
     )
 )
 
+jobs.extend(
+    arc(
+        80,19,"#2c2c2c","#cccccc",0.35,
+        validate_date(f"2018-10-04"), 
+        validate_date(f"2018-12-17"),
+        "2","job_2"
+    )
+)
+
+jobs.extend(
+    arc(
+        80,19,"#2c2c2c","#cccccc",0.35,
+        validate_date(f"2017-12-07"), 
+        validate_date(f"2018-01-26"),
+        "3","job_3"
+    )
+)
+
+jobs.extend(
+    arc(
+        80,19,"#2c2c2c","#cccccc",0.35,
+        validate_date(f"2013-06-10"),
+        validate_date(f"2014-06-10"),
+        "Barona IT","job_4"
+    )
+)
+
+jobs.extend(
+    arc(
+        75.5,9,"#2c2c2c","#cccccc",0.35,
+        validate_date(f"2023-07-01"),
+        validate_date(f"2023-09-23"),
+        "5","job_5"
+    )
+)
+
+jobs.extend(
+    arc(
+        75.5,9,"#2c2c2c","#cccccc",0.35,
+        validate_date(f"2022-06-14"),
+        validate_date(f"2023-06-14"),
+        "Lähitaksi","job_6"
+    )
+)
+
+
+elements.extend(jobs)
+
+# Schools arcs
+schools = []
+schools.extend(
+    arc(
+            60,19,"#2c2c2c","#cccccc",0.65,
+            validate_date(f"2024-03-06"), 
+            validate_date(f"2025-04-19"),
+            "",f"school_1"
+        )
+)
+
+schools.extend(
+    arc(
+            60,19,"#2c2c2c","#cccccc",0.65,
+            validate_date(f"2015-08-19"), 
+            validate_date(f"2023-03-21"),
+            "Metropolia ammattikorkeakoulu",f"school_2"
+        )
+)
+elements.extend(schools)
 
 canvas = svg.SVG(
     viewBox=svg.ViewBoxSpec(-max_x, -max_y, 2 * max_x, 2 * max_y),
